e2a8482485c3aa415938629e1fdb176f992a964c
DavidA-go <juandavidmartinezuwu@gmail.com>
2024-05-14 09:05:31 -0500

    Proyecto

---
+++ proyectotienda/src/main/java/uts/java/proyectotienda/ProyectotiendaApplication.java
@@ -0,0 +1,13 @@
+package uts.java.proyectotienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectotiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectotiendaApplication.class, args);
+	}
+
+}
---
+++ proyectotienda/src/test/java/uts/java/proyectotienda/ProyectotiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package uts.java.proyectotienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectotiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
